0bc16ee65c34b31ba873aa926c5156ab59833a9f
Maks <makzylinski@gmail.com>
2026-09-21 10:58:23 +0200

    controller

--- src/main/java/com/max/JobApp/JobController.java
+++ src/main/java/com/max/JobApp/JobController.java
@@ -1,8 +1,13 @@
 package com.max.JobApp;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class JobController {
 
+    @RequestMapping({"/", "home"})
+    public String home() {
+        return "home";
+    }
 }
