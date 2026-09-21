43e733d02480aa34b9c4ae5ef581ad061c23afef
Maks <makzylinski@gmail.com>
2026-09-21 11:27:38 +0200

    new service

---
+++ src/main/java/com/max/JobApp/service/JobService.java
@@ -0,0 +1,18 @@
+package com.max.JobApp.service;
+
+import com.max.JobApp.model.JobPost;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class JobService {
+
+    public void addJob() {
+
+    }
+
+    public List<JobPost> getAllJobs() {
+
+    }
+}
