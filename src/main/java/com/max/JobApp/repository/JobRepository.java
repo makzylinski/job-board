95ac443ffb4536330cbd290f0640c155841c2eb0
Maks <makzylinski@gmail.com>
2026-09-21 11:31:16 +0200

    repo methods

--- src/main/java/com/max/JobApp/repository/JobRepository.java
+++ src/main/java/com/max/JobApp/repository/JobRepository.java
@@ -11,7 +11,6 @@ public class JobRepository {
     List<JobPost> jobs =  new ArrayList<>(Arrays.asList(
             new JobPost(1, "Java Developer", "Must have good experience in core Java and advanced Java", 2,
                     List.of("Core Java", "J2EE", "Spring Boot", "Hibernate")),
-
             new JobPost(2, "Frontend Developer", "Experience in building responsive web applications using React", 3,
                     List.of("HTML", "CSS", "JavaScript", "React")),
             new JobPost(3, "Data Scientist", "Strong background in machine learning and data analysis", 4,
@@ -24,5 +23,9 @@ public class JobRepository {
 
     public List<JobPost> getAllJobs() {
         return jobs;
+    }
+
+    public void addJob(JobPost job) {
+        jobs.add(job);
     }
 }
